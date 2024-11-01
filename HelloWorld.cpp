e3df4c9d4f428dc6335704bfeb5ee2f2b6458d8f
GEOEGII555 <geoegii2001555@gmail.com>
2024-11-01 17:25:45 +0300

    Initial commit

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,20 +1,116 @@
-﻿// HelloWorld.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <cstdint>
+#include <Windows.h>
 
-#include <iostream>
+struct STDOutHandleGetStdHandleArgumentReturner {
+    DWORD getGetStdHandleArgument() {
+        return STD_OUTPUT_HANDLE;
+    }
+};
+
+struct STDOutHandleReturner {
+    HANDLE getHandle() {
+        return GetStdHandle(STDOutHandleGetStdHandleArgumentReturner().getGetStdHandleArgument());
+    }
+};
+
+struct Character {
+private:
+    uint8_t value;
+public:
+    Character(): Character(0) {}
+    Character(uint8_t value): value(value) {}
+    uint8_t getValue() {
+        return value;
+    }
+    void setValue(uint8_t value) {
+        this->value = value;
+    }
+};
+
+struct CharacterPrinter {
+private:
+    Character character;
+    STDOutHandleReturner stdOutHandleReturner;
+public:
+    CharacterPrinter(Character character, STDOutHandleReturner stdOutHandleReturner): character(character), stdOutHandleReturner(stdOutHandleReturner) {}
+    Character getCharacter() {
+        return character;
+    }
+    void setCharacter(Character character) {
+        this->character = character;
+    }
+    void print() {
+        uint8_t data = character.getValue();
+        WriteConsoleA(stdOutHandleReturner.getHandle(), &data, 1, nullptr, 0);
+    }
+};
+
+struct String {
+private:
+    Character* characters;
+    size_t size;
+public:
+    String(uint8_t* data, size_t size) {
+        characters = new Character[0];
+        this->size = 0;
+        for (size_t i = 0; i < size; i++) {
+            this->push_back(Character(data[i]));
+        }
+        if (size != this->size) abort();
+    }
+
+    ~String() {
+        delete[] characters;
+    }
+
+    void push_back(Character character) {
+        size_t oldSize = size;
+        size_t newSize = oldSize + 1;
+        Character* newCharacters = new Character[newSize];
+        for (size_t i = 0; i < oldSize; i++) {
+            newCharacters[i] = characters[i];
+        }
+        newCharacters[oldSize] = character;
+        delete[] characters;
+        this->size = newSize;
+        this->characters = newCharacters;
+    }
+
+    void print() {
+        for (size_t i = 0; i < size; i++) {
+            STDOutHandleReturner stdOutHandleReturner;
+            CharacterPrinter characterPrinter(characters[i], stdOutHandleReturner);
+            characterPrinter.print();
+        }
+    }
+};
+
+struct CStringStrLen {
+    size_t strlen(uint8_t* data) {
+        size_t i = 0;
+        while (data[i] != '\0') {
+            i++;
+        }
+        return i;
+    }
+};
+
+struct HelloWorldStringFactory {
+private:
+    uint8_t actualData[15] = "Hello, world!\0";
+public:
+
+    String constructString() {
+        CStringStrLen strLen;
+        size_t size = strLen.strlen(actualData);
+        return String(actualData, size);
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HelloWorldStringFactory helloWorldStringFactory;
+    String string = helloWorldStringFactory.constructString();
+    string.print();
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ README.md
@@ -0,0 +1,31 @@
+# Hello World
+"Hello World" is an enterprise level and military grade no nonsense fast compact program for outputting "Hello, World" on your screen.
+
+## Usage
+
+### Windows
+1. Get Visual Studio
+1. Open project
+1. Click "Run"
+
+### Linux
+1. No.
+
+### MacOS
+1. No.
+
+## How is it useful?
+~this is a joke project~ "Hello World" is a very useful introductery piece of code for testing computer systems, compilators, blockchain technology, Web3, and more. If this program fails to run, then you know that your computer is broken.
+
+## Contributing
+As contributors and maintainers of this project, we pledge to respect all people who contribute through reporting issues, posting feature requests, updating documentation, submitting pull requests or patches, and other activities.
+
+We are committed to making participation in this project a harassment-free experience for everyone, regardless of level of experience, gender, gender identity and expression, sexual orientation, disability, personal appearance, body size, race, ethnicity, age, or religion.
+
+Examples of unacceptable behavior by participants include the use of sexual language or imagery, derogatory comments or personal attacks, trolling, public or private harassment, insults, or other unprofessional conduct.
+
+Project maintainers have the right and responsibility to remove, edit, or reject comments, commits, code, wiki edits, issues, and other contributions that are not aligned to this Code of Conduct. Project maintainers who do not follow the Code of Conduct may be removed from the project team.
+
+This code of conduct applies both within project spaces and in public spaces when an individual is representing the project or its community.
+
+Instances of abusive, harassing, or otherwise unacceptable behavior may be reported by opening an issue.
